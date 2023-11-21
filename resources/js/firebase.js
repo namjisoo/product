0f63def9292366dfe7702a494c3fee6d068de742
namjisoo <jisoo39@kakao.com>
2023-11-21 13:36:58 +0900

    firebase

---
+++ resources/js/firebase.js
@@ -0,0 +1,41 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  setDoc,
+  addDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMtpCca1X38fTtOhL4xmucRZ57tNWIm_s",
+  authDomain: "project1-76ca4.firebaseapp.com",
+  projectId: "project1-76ca4",
+  storageBucket: "project1-76ca4.appspot.com",
+  messagingSenderId: "431793577192",
+  appId: "1:431793577192:web:217a920a4759cd67bebd5d",
+  measurementId: "G-TMHHLTVGDH",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+async function getDatas(collectionName) {
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  return querySnapshot;
+}
+
+async function addDatas(collectionName, dataObj) {
+  // 문서 ID 부여
+  //   await setDoc(doc(db, "member", "member1"), { dataObj });
+
+  // 문서 ID 자동
+  await addDoc(collection(db, collectionName), dataObj);
+}
+
+export { db, getDocs, collection, getDatas, setDoc, addDoc, doc, addDatas };
